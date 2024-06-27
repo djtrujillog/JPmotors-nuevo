840b57638ee73887f468c2ffa0528f535700c338
djtrujillog <diegojav_89@hotmail.com>
2024-06-27 15:00:42 -0600

    27062024
    reparacion saber que putas

--- jpmotors-nuevo/src/components/AutoImage.js
+++ jpmotors-nuevo/src/components/AutoImage.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+// src/components/AutoImage.js
+import React, { useState, useEffect } from 'react';
 import { Buffer } from 'buffer';
-function AutoImage({longBlobData, alt}) {
-    const img = new Image();
-    img.src = `data:image/jpeg;base64,${Buffer.from(longBlobData).toString('base64')}`;
 
-    console.log('aqui estoy' + img);
+function AutoImage({ longBlobData, alt }) {
+    const [imgSrc, setImgSrc] = useState('');
+
+    useEffect(() => {
+        if (longBlobData) {
+            try {
+                if (typeof Buffer !== 'undefined') {
+                    const imgData = `data:image/jpeg;base64,${Buffer.from(longBlobData).toString('base64')}`;
+                    setImgSrc(imgData);
+                } else {
+                    console.error('Buffer is undefined.');
+                }
+            } catch (error) {
+                console.error('Error converting longBlobData to base64:', error);
+            }
+        }
+    }, [longBlobData]);
 
     return (
         <div>
-            {(
+            {imgSrc && (
                 <img
-                    src={img.src}
+                    src={imgSrc}
                     alt={alt}
-                    style={{width:'200px', height:'auto'}}                   
+                    style={{ width: '200px', height: 'auto' }}
                 />
             )}
         </div>

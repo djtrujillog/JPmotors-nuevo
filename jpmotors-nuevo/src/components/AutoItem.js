ef6c560d6e6372b2907fd242bf727c23b53113e6
Diego Trujillo <diegojav_89@hotmail.com>
2024-09-09 22:27:40 -0600

    09092024
    Cambios realizados

--- jpmotors-nuevo/src/components/AutoItem.js
+++ jpmotors-nuevo/src/components/AutoItem.js
@@ -6,7 +6,7 @@ const AutoItem = ({ auto, onClick }) => {
   const imagen = auto?.Imagen || 'defaultImage.jpg'; // Usa una imagen predeterminada si no hay imagen
   const marca = auto?.Marca || 'N/A';
   const modelo = auto?.Modelo || 'N/A';
-  const vehiculoID = auto?.VehiculoID; // Se necesita para la lógica del clic
+  // const vehiculoID = auto?.VehiculoID; // Se necesita para la lógica del clic
 
   return (
     <div className="col-md-4 mb-4">
